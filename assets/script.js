0ab986c8d7a03c28471fea033c7fe2e4505b6213
[Dan <123705636+dkopac@users.noreply.github.com>
2023-04-03 16:53:51 -0400

    updates to JS and HTML

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
 var apiKey = "4addb47b0367cdd6214ddfcc93ce9834";
+var city = "";
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("clear-history");
+var currentCity = $("selected-city");
+var currentTemperature = $("temperature");
+var currentWindSpeed = $("wind-speed");
+var currentHumidity = $("humidity");
 function getCurrentWeather(city) {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
@@ -19,6 +27,19 @@ function getFiveDayForecast(city) {
   )
     .then((response) => response.json())
     .then((data) => console.log(data));
+  var date = new Date(
+    response.list[(i + 1) * 8 - 1].dt * 1000
+  ).toLocaleDateString();
+  var iconcode = response.list[(i + 1) * 8 - 1].weather[0].icon;
+  var iconurl = "http://openweathermap.org/img/wn/" + iconcode + ".png";
+  var tempK = response.list[(i + 1) * 8 - 1].main.temp;
+  var tempF = ((tempK - 273.5) * 1.8 + 32).toFixed(2);
+  var humidity = response.list[(i + 1) * 8 - 1].main.humidity;
+
+  $("#fDate" + 1).html(date);
+  $("#fImg" + i).html("<img src=" + iconurl + ">");
+  $("#fTemp" + i).html(tempF + "&#8457");
+  $("fHumidity" + i).html(humidity + "%");
 }
 
 var citySearchForm = $(".city");

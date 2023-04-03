2e7d689e86498b19aeb30867b4430ef21a092841
[Dan <123705636+dkopac@users.noreply.github.com>
2023-04-03 16:26:45 -0400

    restarted assignment

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var apiKey = "4addb47b0367cdd6214ddfcc93ce9834";
+function getFiveDayForecast(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+function getCurrentWeather(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      $("header h1").html(`
+      ${new Date().toDateString()}
+      <img src="http://openweathermap.org/img/w/${
+        data.weather[0].icon
+      }.png"/>`);
+    });
+}
+
+var citySearchForm = $(".city");
+citySearchForm.submit(function (event) {
+  event.preventDefault();
+  var city = $("#city").val();
+  getCurrentWeather(city);
+});

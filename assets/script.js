520b6b7877d7e3dea6fdef20eb4e08a68a4c15dd
[Dan <123705636+dkopac@users.noreply.github.com>
2023-04-03 22:46:59 -0400

    updated

--- assets/script.js
+++ assets/script.js
@@ -7,27 +7,41 @@ var currentCity = $("selected-city");
 var currentTemperature = $("temperature");
 var currentWindSpeed = $("wind-speed");
 var currentHumidity = $("humidity");
+var cityEle = $("#city");
+function generateHtml(lat, lon, city) {
+  console.log(cityEle);
+  cityEle.text(city);
+}
+
 function getCurrentWeather(city) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}
+    `
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      $("header h1").html(`
+      let obj = data[0];
+      let lat = obj.lat;
+      let lon = obj.lon;
+      generateHtml(lat, lon, city);
+      getFiveDayForecast(lat, lon);
+      /*$("header h1").html(`
       ${new Date().toDateString()}
       <img src="http://openweathermap.org/img/w/${
         data.weather[0].icon
-      }.png"/>`);
+      }.png"/>`);*/
     });
 }
-function getFiveDayForecast(city) {
+function getFiveDayForecast(lat, lon) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}
+    `
   )
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
+      let weekAheadEle = document.querySelector("#week-ahead-container");
       for (let i = 0; i < response.list.length; i = i + 8) {
         var date = new Date(response.list[i].dt * 1000).toLocaleDateString();
         var iconcode = response.list[i].weather[0].icon;
@@ -35,7 +49,13 @@ function getFiveDayForecast(city) {
         var tempK = response.list[i].main.temp;
         var tempF = ((tempK - 273.5) * 1.8 + 32).toFixed(2);
         var humidity = response.list[i].main.humidity;
-
+        var card = document.createElement("div");
+        var cardHeader = document.createElement("h1");
+        cardHeader.textContent = response.list[i].dt_txt.substring(0, 10);
+        card.append(cardHeader);
+        weekAheadEle.append(card);
+        console.log(tempF);
+        console.log(date);
         $("#fDate" + i).html(date);
         $("#fImg" + i).html("<img src=" + iconurl + ">");
         $("#fTemp" + i).html(tempF + "&#8457");
@@ -47,7 +67,7 @@ function getFiveDayForecast(city) {
 var citySearchForm = $(".city");
 citySearchForm.submit(function (event) {
   event.preventDefault();
-  getFiveDayForecast(city);
+  //getFiveDayForecast(city);
   var city = $("#city").val();
   getCurrentWeather(city);
 });
